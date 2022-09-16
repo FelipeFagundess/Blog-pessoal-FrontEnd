6f6511d3b79b3635ec8f1fa4efb3cf8196237c8c
felipe fagundes <felipefagundesb21@hotmail.com>
2022-09-16 11:39:53 -0300

    adicionando redux, ajeitando tema e home

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,67 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/estaticos/navbar/Navbar";
-import Footer from "./components/estaticos/footer/Footer";
-import Home from "./paginas/home/Home";
-import CadastraUsuario from "./paginas/cadastraUsuario/CadastraUsuario";
+import React from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from "./components/statics/footer/Footer";
 import Login from "./paginas/login/Login";
-import "./App.css";
-import ListaTema from "./components/temas/listatemas/ListaTema";
-import ListaPostagem from "./components/postagens/listapostagem/ListaPostagem";
+import Home from "./paginas/home/Home";
+import ListaTema from './components/temas/listaTema/ListaTema';
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPostagem/cadastroPostagem';
+import CadastroTema from './components/temas/cadastroTema/cadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/deletarPostagem';
+import DeletarTema from './components/temas/deletarTema/deletarTema';
+import { Provider } from 'react-redux';
+import store from './store/store';
+
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/cadastrausuario" element={<CadastraUsuario />} />
-        <Route path="/temas" element={<ListaTema />} />
-        <Route path="/posts" element={<ListaPostagem />} />
-      </Routes>
-      <Footer />
-    </Router>
+
+    <Provider store={store}>
+
+      <ToastContainer />
+
+      <Router>
+
+        <Navbar />
+
+        <Routes>
+
+          <Route path='/' element={<Login />}></Route>
+
+          <Route path='/login' element={<Login />}></Route>
+
+          <Route path='/register' element={<Login />}></Route>
+
+          <Route path='/home' element={<Home />}></Route>
+
+          <Route path='/temas' element={<ListaTema />}></Route>
+
+          <Route path='/postagens' element={<ListaPostagem />}></Route>
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+        </Routes>
+
+        <Footer />
+
+      </Router>
+    </Provider>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,10 +1,37 @@
-import React from "react";
-import { Typography, Grid, Button } from "@material-ui/core";
-import "./Home.css";
-import { Box } from "@mui/material";
-import TabPostagem from "../../components/postagens/tabpostagens/TabPostagem";
+
+import React, { useEffect } from 'react';
+import { Typography, Box, Grid, Button } from '@mui/material';
+import TabPostagem from '../../components/postagens/tabPostagem/TabPostagem';
+import { Link, useNavigate } from 'react-router-dom';
+import ModalPostagem from '../../components/postagens/modalPostagem/modalPostagem';
+import './Home.css';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../store/tokens/tokens-reducer';
+import { toast } from 'react-toastify';
 
 function Home() {
+
+  let navigate = useNavigate();
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+      (state) => state.tokens
+  );
+
+  useEffect(() => {
+      if (token == "") {
+          toast.info('Você precisa estar logado.', {
+              position: "top-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              });
+          navigate("/login")
+
+      }
+  }, [token])
+
   return (
     <>
       
